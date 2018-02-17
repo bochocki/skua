8266d510806858f2c5475bdae9aafd685a70a774
Brad Ochocki <brad.ochocki@gmail.com>
2018-02-16 20:24:19 -0800

    only single quotes

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -5,8 +5,8 @@ var storage = chrome.storage.local;
 var reload = setInterval(skua_filter, 500);
 
 // add classes for skua buttons
-var trollButton = $("#skua-troll");
-var notTrollButton = $("#skua-notTroll");
+var trollButton = $('#skua-troll');
+var notTrollButton = $('#skua-notTroll');
 
 // variable to store the slider value
 var sliderValue;
@@ -20,7 +20,7 @@ if(typeof userid == 'undefined') {
 // a function to remove text between brackets
 function removeBrackets(input) {
   return input
-    .replace(/<.*?>/g, "");
+    .replace(/<.*?>/g, '');
 }
 
 // a function to implement filtering
@@ -31,24 +31,24 @@ function skua_filter() {
       sliderValue = items.aValue;
 
       // get all tweets on the current page
-      var tweets = document.getElementsByClassName("tweet-text");
+      var tweets = document.getElementsByClassName('tweet-text');
 
       // get all tweets tagged with `skua-tweet`
-      var skua_tweets = document.getElementsByClassName("skua-tweet");
+      var skua_tweets = document.getElementsByClassName('skua-tweet');
 
       // if there are unprocessed tweets, find them and process them
       if (tweets.length != skua_tweets.length)
       {
-        console.log("running skua")
+        console.log('running skua')
         for(var i=0; i <tweets.length; i++)
         {
 
         // if the current tweet has the `skua-tweet` class, it's been processed
         // already, so do nothing. Otherwise, process the tweet.
-        if (tweets[i].classList.contains("skua-tweet")) {
+        if (tweets[i].classList.contains('skua-tweet')) {
         } else {
           // add the `skua-tweet` class
-          tweets[i].className += " skua-tweet";
+          tweets[i].className += ' skua-tweet';
 
           // make new data elements to store the `skua_score` and `user_score`
           $(tweets[i]).parents('.tweet').data('skua_score')
@@ -58,35 +58,35 @@ function skua_filter() {
           var clean_text = removeBrackets(tweets[i].innerHTML);
 
           // send the clean_text to the skua API
-          $.get("https://www.skua.online/CleverBird", { tweet: clean_text, element: i })
+          $.get('https://www.skua.online/CleverBird', { tweet: clean_text, element: i })
             .done(function( result ) {
               // update the tweet background based on the skua_score
               $(tweets[result.element]).parents('.tweet').css('background', result.color);
 
               // save the skua_score as data for the element
               $(tweets[result.element]).parents('.tweet').data('skua_score', result.score);
-            }, "json");
+            }, 'json');
 
             // add troll button button if it doesn't already exist.
-            if ($(tweets[i]).parents('.tweet').find( ".skua-troll" )[0]) {
+            if ($(tweets[i]).parents('.tweet').find( '.skua-troll' )[0]) {
             } else {
-              $(tweets[i]).parents('.tweet').find("div.dropdown-menu ul").prepend('<li class="skua-troll" role="presentation"> <button type="button" class="dropdown-link" role="menuitem">Troll (Skua)</button></li>');
+              $(tweets[i]).parents('.tweet').find('div.dropdown-menu ul').prepend('<li class='skua-troll' role='presentation'> <button type='button' class='dropdown-link' role='menuitem'>Troll (Skua)</button></li>');
             }
 
             // add notTroll button if it doesn't already exist.
-            if ($(tweets[i]).parents('.tweet').find( ".skua-notTroll" )[0]) {
+            if ($(tweets[i]).parents('.tweet').find( '.skua-notTroll' )[0]) {
             } else {
-              $(tweets[i]).parents('.tweet').find("div.dropdown-menu ul").prepend('<li class="skua-notTroll" role="presentation"> <button type="button" class="dropdown-link" role="menuitem">Not Troll (Skua)</button></li>');
+              $(tweets[i]).parents('.tweet').find('div.dropdown-menu ul').prepend('<li class='skua-notTroll' role='presentation'> <button type='button' class='dropdown-link' role='menuitem'>Not Troll (Skua)</button></li>');
             }
 
             // click action for troll button
-            $(tweets[i]).parents('.tweet').find( ".skua-troll" ).click(function() {
+            $(tweets[i]).parents('.tweet').find( '.skua-troll' ).click(function() {
               // set tweet background and update user_score
               $(this).parents('.tweet').css('background', 'rgb(222, 45, 38)');
               $(this).parents('.tweet').data('user_score', 100);
 
               // log tweet in database
-              $.get("https://www.skua.online/SkuaLogging", {
+              $.get('https://www.skua.online/SkuaLogging', {
                 tweet: removeBrackets($(this).parents('.tweet').find('.tweet-text').text()),
                 userid: userid,
                 tweeterid: $(this).parents('.tweet').attr('data-user-id'),
@@ -95,12 +95,12 @@ function skua_filter() {
             });
 
             // click action for notTroll button
-            $(tweets[i]).parents('.tweet').find( ".skua-notTroll" ).click(function() {
+            $(tweets[i]).parents('.tweet').find( '.skua-notTroll' ).click(function() {
               // set tweet background and update user_score
               $(this).parents('.tweet').css('background', 'white');
               $(this).parents('.tweet').data('user_score', 0);
               // log tweet in database
-              $.get("https://www.skua.online/SkuaLogging", {
+              $.get('https://www.skua.online/SkuaLogging', {
                 tweet: removeBrackets($(this).parents('.tweet').find('.tweet-text').text()),
                 userid: userid,
                 tweeterid: $(this).parents('.tweet').attr('data-user-id'),
