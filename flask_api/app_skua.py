397c5e2076013817c436ccc4e5d520af3ac813ad
Brad Ochocki <brad.ochocki@gmail.com>
2018-02-16 20:55:56 -0800

    debugging 500

--- flask_api/app_skua.py
+++ flask_api/app_skua.py
@@ -45,8 +45,9 @@ def preprocess(tweet):
     tweet = re.sub(mention_regex, ' ', tweet)  # mentions
     tweet = re.sub(r'[^\w\s]', ' ',tweet)  #punctuation
     tweet = re.sub(r'\s+', ' ', tweet) # extra whitespace
-    print('ok')
     tweet = tweet.strip() # leading/trailing whitespace
+
+    print('\nok')
 
     ## TODO: REMOVE pic.twitter.com
 
